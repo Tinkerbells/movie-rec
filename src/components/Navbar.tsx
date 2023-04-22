3b640a625193044b80cb5f0453b06bccbc1fe982
untermensch <untermenschH@protonmail.com>
2023-04-22 18:23:37 +0300

    change navbar size

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ import { ThemeToggle } from "./ThemeToggle";
 export const Navbar = () => {
   const { data: session } = useSession();
   return (
-    <div className="fixed left-0 right-0 top-0 z-50 flex h-20 items-center justify-between border-b border-accent px-5">
+    <div className="fixed left-0 right-0 top-0 z-50 flex h-16 items-center justify-between border-b border-accent px-5">
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-3xl">
         MOVIE-REC
       </h1>
