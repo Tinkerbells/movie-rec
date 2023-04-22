d91a1538858ef90e68199572beef293eb0315702
untermensch <untermenschH@protonmail.com>
2023-04-22 15:41:36 +0300

    add logo and align other content

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,17 +6,21 @@ import { ThemeToggle } from "./ThemeToggle";
 export const Navbar = () => {
   const { data: session } = useSession();
   return (
-    <div className="fixed left-0 right-0 top-0 z-50 flex h-20 items-center justify-between border-b border-accent">
-      <div></div>
-      <div>
-        {!session ? (
-          // Handle unauthenticated state, e.g. render a SignIn component
-          <SignInButton />
-        ) : (
-          <SignOutButton />
-        )}
+    <div className="fixed left-0 right-0 top-0 z-50 flex h-20 items-center justify-between border-b border-accent px-5">
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-3xl">
+        MOVIE-REC
+      </h1>
+      <div className="flex items-center justify-between gap-8">
+        <div>
+          {!session ? (
+            // Handle unauthenticated state, e.g. render a SignIn component
+            <SignInButton />
+          ) : (
+            <SignOutButton />
+          )}
+        </div>
+        <ThemeToggle />
       </div>
-      <ThemeToggle />
     </div>
   );
 };
