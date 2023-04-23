d0f129b1bb06810dfab76697b7c0014d290f4a7a
untermensch <untermenschH@protonmail.com>
2023-04-23 21:08:29 +0300

    fix sign in button

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -1,11 +1,11 @@
 import { ReactNode } from "react";
 import { Button } from "./ui/button";
-import { signIn, useSession } from "next-auth/react";
+import { signIn } from "next-auth/react";
 
-export const SignInButton = ({ children }: { children: ReactNode }) => {
+export const SignInButton = () => {
   return (
-    <Button variant="outline" onClick={() => signIn()}>
-      {children}
+    <Button variant="outline" className="max-w-sm" onClick={() => signIn()}>
+      Sign in
     </Button>
   );
 };
