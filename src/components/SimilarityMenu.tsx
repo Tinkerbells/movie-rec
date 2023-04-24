8393fd2a448e81282a0c4a7dcb9c1880edee293a
untermensch <untermenschH@protonmail.com>
2023-04-24 15:34:10 +0300

    add form to similarity menu

--- src/components/SimilarityMenu.tsx
+++ src/components/SimilarityMenu.tsx
@@ -12,83 +12,118 @@ import { Label } from "./ui/label";
 import { Input } from "./ui/input";
 import { Plus, XCircle } from "lucide-react";
 import { Button } from "./ui/button";
+import { useForm } from "@/hooks";
+import { toast } from "./ui/use-toast";
+import { exampleRouter } from "@/server/api/routers/example";
 
+interface GenreQueryFormValues {
+  favorites: string[];
+}
 export const SimilarityMenu = () => {
+  const { values, errors, setFieldValue, handleSumbit } =
+    useForm<GenreQueryFormValues>({
+      defaultValues: { favorites: [""] },
+      onSubmit: (values) => {
+        console.log(values);
+      },
+      validate: (values) => {
+        const isEmpty = values.favorites
+          ? values.favorites.find((el) => el.length === 0)
+          : true;
+        if (!isEmpty) {
+          return { favorites: "No favorite movies are provided!" };
+        }
+        return;
+      },
+    });
+
   const favoriteNumbers = ["First", "Second", "Third", "Fourth", "Fifth"];
-  const [favorites, setFavorites] = useState<string[]>([""]);
   const changeFavorite = (
     event: React.ChangeEvent<HTMLInputElement>,
     index: number
   ) => {
-    const temp = favorites;
+    const temp = values.favorites;
     temp[index] = event.target.value;
-    setFavorites(temp);
+    setFieldValue("favorites", temp);
   };
   const removeFavorite = (index: number) => {
-    if (favorites.length > 1) {
-      setFavorites(favorites.filter((_, i) => i !== index));
+    if (values.favorites.length > 1) {
+      setFieldValue(
+        "favorites",
+        values.favorites.filter((_, i) => i !== index)
+      );
     }
   };
   return (
     <TabsContent value="similar">
-      <Card>
-        <CardHeader>
-          <CardTitle>Similarity recommendation</CardTitle>
-          <CardDescription>
-            Get a recommendation by typing favorite movies
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <div className="flex flex-col items-center gap-4 rounded-lg border border-accent p-4">
-            {favorites.map((_, index) => (
-              <div
-                className="grid w-full max-w-xs items-center gap-1.5"
-                key={index}
-              >
-                <Label htmlFor={`favorite-${index}`}>
-                  {favoriteNumbers[index]} favorite
-                </Label>
-                <div className="flex">
-                  <Input
-                    type="text"
-                    id={`favorite-${index}`}
-                    className={`${favorites.length > 1 && "pr-9"}`}
-                    placeholder="Type your favorite movie..."
-                    onChange={(e) => {
-                      changeFavorite(e, index);
-                    }}
-                  />
-                  {favorites.length > 1 ? (
-                    <button
-                      type="button"
-                      className="group -ml-7"
-                      onClick={() => removeFavorite(index)}
-                    >
-                      <XCircle className="h-5 w-5 text-accent group-hover:text-primary" />
-                    </button>
+      <form onSubmit={handleSumbit}>
+        <Card>
+          <CardHeader>
+            <CardTitle>Similarity recommendations</CardTitle>
+            <CardDescription>
+              Get a recommendations by typing your favorite movies
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="flex flex-col items-center gap-4 rounded-lg border border-accent p-4">
+              {values.favorites.map((el, index) => (
+                <div
+                  className="grid w-full max-w-xs items-center gap-1.5"
+                  key={index}
+                >
+                  <Label htmlFor={`favorite-${index}`}>
+                    {favoriteNumbers[index]} favorite
+                  </Label>
+                  <div className="flex">
+                    <Input
+                      type="text"
+                      id={`favorite-${index}`}
+                      className={`${values.favorites.length > 1 && "pr-9"}`}
+                      placeholder="Type your favorite movie..."
+                      onChange={(e) => {
+                        changeFavorite(e, index);
+                      }}
+                    />
+                    {values.favorites.length > 1 ? (
+                      <button
+                        type="button"
+                        className="group -ml-7"
+                        onClick={() => removeFavorite(index)}
+                      >
+                        <XCircle className="h-5 w-5 text-accent group-hover:text-primary" />
+                      </button>
+                    ) : null}
+                  </div>
+                  {!!errors && el.length === 0 ? (
+                    <p className="text-sm text-destructive">
+                      {errors.favorites}
+                    </p>
                   ) : null}
                 </div>
-              </div>
-            ))}
-            {/* Set max count of favorites movies to 5 */}
-            {favorites.length <= 4 ? (
-              <Button
-                variant="outline"
-                className="w-10 rounded-full p-0"
-                onClick={() => setFavorites([...favorites, ""])}
-              >
-                <Plus className="h-4 w-4" />
-                <span className="sr-only">Open popover</span>
-              </Button>
-            ) : null}
-          </div>
-        </CardContent>
-        <CardFooter>
-          <Button variant={"outline"} className="w-full" type="submit">
-            Get recommendation
-          </Button>
-        </CardFooter>
-      </Card>
+              ))}
+              {/* Set max count of favorites movies to 5 */}
+              {values.favorites.length <= 4 ? (
+                <Button
+                  variant="outline"
+                  className="w-10 rounded-full p-0"
+                  type="button"
+                  onClick={() =>
+                    setFieldValue("favorites", [...values.favorites, ""])
+                  }
+                >
+                  <Plus className="h-4 w-4" />
+                  <span className="sr-only">Open popover</span>
+                </Button>
+              ) : null}
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button variant={"outline"} className="w-full" type="submit">
+              Get recommendation
+            </Button>
+          </CardFooter>
+        </Card>
+      </form>
     </TabsContent>
   );
 };
