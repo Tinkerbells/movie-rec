2c246c9d3e0bfa8eb93b0ac227ef68f1fbf06021
untermensch <untermenschH@protonmail.com>
2023-04-25 14:19:26 +0300

    add tmdb api key

--- src/env.mjs
+++ src/env.mjs
@@ -21,6 +21,7 @@ const server = z.object({
   // Add `.min(1) on ID and SECRET if you want to make sure they're not empty
   GITHUB_CLIENT_ID: z.string(),
   GITHUB_CLIENT_SECRET: z.string(),
+  TMDB_API_KEY: z.string(),
 });
 
 /**
@@ -45,6 +46,7 @@ const processEnv = {
   GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
   GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
   OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  TMDB_API_KEY: process.env.TMDB_API_KEY,
   // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
 };
 
