65e514296cc2cb8455fe58103a64e7b4f62bd60c
untermensch <untermenschH@protonmail.com>
2023-04-25 19:29:40 +0300

    update movie card with blur image and other stuff

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -7,8 +7,12 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Image as ImageIcon, Loader2 } from "lucide-react";
 import Image from "next/image";
 import { api } from "@/utils/api";
+import { BlurImage } from "./BlurImage";
+import { TmdbImageLoader } from "./TmdbImageLoader";
+import { formatDate } from "@/helpers";
 interface MovieCardProps {
   movie: RecommendationType;
 }
@@ -17,27 +21,48 @@ export const MovieCard: FC<MovieCardProps> = ({ movie }) => {
     query: movie.title,
   });
   return (
-    <>
+    <Card key={movie.title} className="flex w-full max-w-xl items-center">
       {isLoading ? (
-        <p>Loading...</p>
+        <div className="h-32 p-2">
+          <div className="flex h-full w-20 items-center justify-center rounded-lg bg-secondary text-secondary-foreground">
+            <Loader2 className="h-4 w-4 animate-spin" />
+          </div>
+        </div>
       ) : (
-        <Card key={movie.title} className="flex">
-          <CardContent>
-            {data ? (
-              <Image
-                width={100}
-                height={100}
-                alt={movie.title}
-                src={`https://image.tmdb.org/t/p/w300/${data}`}
-              />
-            ) : null}
-          </CardContent>
-          <CardHeader>
-            <CardTitle>{movie.title}</CardTitle>
-            <CardDescription>{movie.description}</CardDescription>
-          </CardHeader>
-        </Card>
+        <>
+          {data?.posterPath ? (
+            <a
+              className="flex cursor-pointer items-center p-2"
+              href={`https://www.themoviedb.org/movie/${data.tmdbId}`}
+            >
+              <figure className="h-full w-20 items-center">
+                <BlurImage
+                  src={data.posterPath}
+                  alt={movie.title}
+                  loader={TmdbImageLoader}
+                  width={300}
+                  height={300}
+                />
+              </figure>
+            </a>
+          ) : (
+            <div className="h-32 p-2">
+              <div className="flex h-full w-20 items-center justify-center rounded-lg bg-secondary text-secondary-foreground">
+                <ImageIcon />
+              </div>
+            </div>
+          )}
+        </>
       )}
-    </>
+      <CardHeader>
+        <CardTitle>
+          {movie.title}
+          <p className="mt-1 text-sm font-normal text-muted-foreground">
+            {formatDate(data?.releaseDate!)}
+          </p>
+        </CardTitle>
+        <CardDescription>{movie.description}</CardDescription>
+      </CardHeader>
+    </Card>
   );
 };
