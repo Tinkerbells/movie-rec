fda5e38e2bfcd2aaba001d4c8a4c3fb931829e7d
untermensch <untermenschH@protonmail.com>
2023-04-25 19:27:42 +0300

    update generate prompt function, hope for better result

--- src/helpers/generatePrompt.ts
+++ src/helpers/generatePrompt.ts
@@ -1,24 +1,23 @@
 export const generateGenrePrompt = (genres: string[], query?: string) => {
-  return `Give me recommendations of 5 movies in this genres ${genres.join(
+  return `Give me recommendations of 3 movies in this genres ${genres.join(
     " "
   )} all comes together ${
-    query && "and this description query - " + `"` + query.trim() + `"`
-  } :
-          [{
-          \"title\": \"Movie title\", 
-          \"description\": \"Movie description in 20 words\"
-          }]
-          The JSON object is:"`;
+    query && "and this description query - " + query.trim()
+  } Provide a  RFC8259 compliant JSON response following this format without deviation.
+[{
+  "title": "movie title",
+  "description": "movie description about 25 words",
+}]
+The JSON response:`;
 };
 
 export const generateSimilarPrompt = (favorites: string[]) => {
-  return `Give me recommendations of 5 movies similar to those ones ${favorites.join(
+  return `Give me recommendations of 3 movies similar to those ones ${favorites.join(
     " "
-  )} 
-  } :
-          [{
-          \"title\": \"Movie title\", 
-          \"description\": \"Movie description in 20 words\"
-          }]
-          The JSON object is:"`;
+  )} Provide a  RFC8259 compliant JSON response following this format without deviation.
+[{
+  "title": "movie title",
+  "description": "movie description about 25 words",
+}]
+The JSON response:`;
 };
