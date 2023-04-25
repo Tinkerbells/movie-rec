bc79ca98a15d19b19b7ba249b815bf80f93ffaeb
untermensch <untermenschH@protonmail.com>
2023-04-25 14:20:16 +0300

    add types to tmdb response

---
+++ src/types/tmdb.ts
@@ -0,0 +1,23 @@
+export interface TmdbResponse {
+  page: number;
+  results: SearchResults[];
+  total_pages: number;
+  total_results: number;
+}
+
+export interface SearchResults {
+  poster_path?: string;
+  adult: boolean;
+  overview: string;
+  release_date: string;
+  genre_ids: number[];
+  id: number;
+  original_title: string;
+  original_language: string;
+  title: string;
+  backdrop_path?: string;
+  popularity: number;
+  vote_count: number;
+  video: boolean;
+  vote_average: number;
+}
