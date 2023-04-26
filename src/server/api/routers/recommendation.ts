49461ee71554295c139f476028ba1debe618f24b
untermensch <untermenschH@protonmail.com>
2023-04-26 14:58:04 +0300

    rewrite recommendation router to work as chat

--- src/server/api/routers/recommendation.ts
+++ src/server/api/routers/recommendation.ts
@@ -5,24 +5,22 @@ import { openai } from "@/lib/opeanai";
 import { generateGenrePrompt, generateSimilarPrompt } from "@/helpers";
 
 export const recommendationRouter = createTRPCRouter({
-  genre: protectedProcedure
-    .input(z.object({ genres: z.string().array(), query: z.string() }))
+  getRecommendations: protectedProcedure
+    .input(
+      z.object({
+        messages: z.object({ role: z.string(), content: z.string() }).array(),
+      })
+    )
     .query(async ({ input }) => {
-      const content = generateGenrePrompt(input.genres, input.query);
       const completion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: content }],
+        // @ts-ignore
+        messages: input.messages,
       });
-      return JSON.parse(completion.data.choices[0]?.message?.content!);
-    }),
-  similar: protectedProcedure
-    .input(z.object({ favorites: z.string().array() }))
-    .query(async ({ input }) => {
-      const content = generateSimilarPrompt(input.favorites);
-      const completion = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: content }],
-      });
-      return JSON.parse(completion.data.choices[0]?.message?.content!);
+      const res = JSON.parse(completion.data.choices[0]?.message?.content!);
+      return {
+        assistantMessage: completion.data.choices[0]?.message,
+        recommendations: res,
+      };
     }),
 });
