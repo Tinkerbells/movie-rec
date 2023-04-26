3a98d8eaeb0540ecd649b536e36b006c1271c49b
untermensch <untermenschH@protonmail.com>
2023-04-26 14:58:24 +0300

    update consts file

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -1,1 +1,28 @@
-export * from "./genres";
+export const genres = [
+  "Action",
+  "Adventure",
+  "Animation",
+  "Biography",
+  "Comedy",
+  "Crime",
+  "Documentary",
+  "Drama",
+  "Family",
+  "Fantasy",
+  "Film-Noir",
+  "History",
+  "Horror",
+  "Music",
+  "Musical",
+  "Mystery",
+  "Romance",
+  "Sci-Fi",
+  "Sport",
+  "Thriller",
+  "War",
+  "Western",
+];
+
+export const RECOMMENDATIONS_COUNT = 5;
+
+export const UPDATE_PROMPT = `Give me another ${RECOMMENDATIONS_COUNT}`;
