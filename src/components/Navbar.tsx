42778f1c91951d5d2ddf9e013dfcc15819d3a1f0
untermensch <untermenschH@protonmail.com>
2023-04-27 23:38:27 +0300

    litte fixes to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,17 +2,14 @@ import { useSession } from "next-auth/react";
 import { SignInButton } from "./SignInButton";
 import { SignOutButton } from "./SignOutButton";
 import { ThemeToggle } from "./ThemeToggle";
-import Link from "next/link";
 
 export const Navbar = () => {
   const { data: session } = useSession();
   return (
     <div className="fixed left-0 right-0 top-0 z-50 flex h-16 items-center justify-between border-b border-accent bg-card px-5">
-      <Link href="/">
-        <h1 className="scroll-m-20 text-2xl font-extrabold tracking-tight md:text-4xl lg:text-3xl">
-          MOVIE-REC
-        </h1>
-      </Link>
+      <h1 className="hidden scroll-m-20 text-2xl font-extrabold tracking-tight md:flex md:text-4xl lg:text-3xl">
+        MOVIE-REC
+      </h1>
       <div className="flex items-center justify-between gap-8">
         <div>
           {!session?.user ? (
