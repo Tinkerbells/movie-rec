1be416008d6cd314106a2a5cc3c8f43484feeafa
untermensch <untermenschH@protonmail.com>
2023-04-27 16:17:48 +0300

    add recommendation page

--- src/pages/recommendations/index.tsx
+++ src/pages/recommendations/index.tsx
@@ -1,57 +1,79 @@
-import { Recommendations } from "@/components";
+import { RecommendationMenu, Recommendations } from "@/components";
 import { ToastAction } from "@/components/ui/toast";
 import { toast } from "@/components/ui/use-toast";
-import { useMessageStore } from "@/store/messagesStore";
+import { generateUpdatePrompt } from "@/helpers";
+import { messageType } from "@/types/message";
+import { RecommendationType } from "@/types/recommendation";
 import { api } from "@/utils/api";
-import { Loader2 } from "lucide-react";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useState } from "react";
 
 const RecommendationsPage = () => {
-  const router = useRouter();
-  const messages = useMessageStore((state) => state.messages);
-
-  const { data, isFetching, isLoading, error, refetch } =
+  const [messages, setMessages] = useState<messageType[]>([]);
+  const [recommendations, setRecommendations] = useState<RecommendationType[]>(
+    []
+  );
+  const { data, isFetching, refetch } =
     api.recommendation.getRecommendations.useQuery(
       {
         messages: messages,
       },
       {
+        onSuccess: (data) => {
+          setRecommendations(data.recommendations);
+        },
         enabled: messages.length >= 1,
+        retry: false,
+        onError: (error) => {
+          toast({
+            variant: "destructive",
+            title: "Something went wrong!",
+            description: error.message,
+            action: (
+              <ToastAction
+                altText="Try again"
+                onClick={() => {
+                  refetch();
+                }}
+              >
+                Try again
+              </ToastAction>
+            ),
+          });
+        },
       }
     );
 
-  if (error) {
-    toast({
-      variant: "destructive",
-      title: "Something went wrong!",
-      description: error.message,
-      action: (
-        <ToastAction altText="Try again" onClick={() => refetch()}>
-          Try again
-        </ToastAction>
-      ),
-    });
-  }
-  // Redirect back to dashboard if no date specified
-  useEffect(() => {
-    if (messages.length < 1) {
-      router.push("/dashboard");
+  const handleUpdate = () => {
+    if (data?.assistantMessage) {
+      console.log(data.assistantMessage);
+      setMessages([
+        ...messages,
+        data.assistantMessage!,
+        { role: "user", content: generateUpdatePrompt() },
+      ]);
     }
-  });
+  };
+
+  const getBack = () => {
+    setMessages([]);
+    setRecommendations([]);
+  };
+
+  const setMessage = (content: string) => {
+    setMessages([{ role: "user", content: content }]);
+  };
 
   return (
     <div className="relative flex h-screen items-center justify-center overflow-x-hidden">
-      {(!isLoading || !isFetching) && data ? (
-        <div className="mb-10 mt-40 flex flex-col gap-4 ">
-          <Recommendations
-            refetch={refetch}
-            recommendations={data.recommendations}
-            assistantMessage={data?.assistantMessage!}
-          />
-        </div>
+      {recommendations.length < 1 && messages.length <= 1 ? (
+        <RecommendationMenu setMessage={setMessage} isLoading={isFetching} />
       ) : (
-        <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+        <Recommendations
+          handleUpdate={handleUpdate}
+          recommendations={recommendations}
+          getBack={getBack}
+          isLoading={isFetching}
+        />
       )}
     </div>
   );
