fdc35c7a4aa4acdafdbf70afae2a5a189c688afb
untermensch <untermenschH@protonmail.com>
2023-04-27 16:30:18 +0300

    update tailwindcss config for build

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -58,12 +58,10 @@ export default {
       },
       keyframes: {
         "accordion-down": {
-          from: { height: 0 },
           to: { height: "var(--radix-accordion-content-height)" },
         },
         "accordion-up": {
           from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
         },
       },
       animation: {
