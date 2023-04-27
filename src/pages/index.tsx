b9ec46b396a0f14a1d0c63f77eb8e409f61a281a
untermensch <untermenschH@protonmail.com>
2023-04-28 00:14:01 +0300

    another mobile styles fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,11 @@ const Home: NextPage = () => {
   return (
     <div className="relative flex h-screen items-center justify-center overflow-x-hidden">
       <div className="flex max-w-5xl flex-col items-center gap-8">
-        <div>
-          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        <div className="flex max-w-[300px] flex-col items-center md:max-w-full">
+          <h1 className="scroll-m-20 text-3xl font-extrabold tracking-tight md:text-4xl lg:text-5xl">
             Find movie recommendations using AI
           </h1>
-          <p className="leading-7 [&:not(:first-child)]:mt-6">
+          <p className="text-center text-sm leading-7 md:text-lg [&:not(:first-child)]:mt-6">
             Looking for the next movie to watch but don't know where to start?
             With this app, you can easily find the next movie to watch by
             picking your favorite genres or find similar movies based on your
@@ -20,7 +20,7 @@ const Home: NextPage = () => {
         <Button
           onClick={() => void signIn()}
           variant={"outline"}
-          className="w-full max-w-sm"
+          className="w-full max-w-xs md:max-w-sm"
         >
           Get started
         </Button>
