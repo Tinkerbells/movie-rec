d7699ce07f9066eb7ae98f2c7aefd1a59184c88b
untermensch <untermenschH@protonmail.com>
2023-04-28 11:43:17 +0300

    rewrite app description to work with series

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,12 +8,12 @@ const Home: NextPage = () => {
       <div className="flex max-w-5xl flex-col items-center gap-8">
         <div className="flex max-w-[300px] flex-col items-center md:max-w-full">
           <h1 className="scroll-m-20 text-3xl font-extrabold tracking-tight md:text-4xl lg:text-5xl">
-            Find movie recommendations using AI
+            Find movie or series recommendations using AI
           </h1>
           <p className="text-center text-sm leading-7 md:text-lg [&:not(:first-child)]:mt-6">
-            Looking for the next movie to watch but don't know where to start?
-            With this app, you can easily find the next movie to watch by
-            picking your favorite genres or find similar movies based on your
+            Looking for the next movie or series to watch but don't know where
+            to start? With this app, you can easily find the next movie to watch
+            by picking your favorite genres or find similar based on your
             favorites.
           </p>
         </div>
